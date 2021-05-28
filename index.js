a1022a4d604d7a9b5917f76aae371e0cfc5b07ea
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-05-28 11:11:11 +0200

    add 404

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.get("/", function (req, res) {
   res.send("hello");
 });
 
+app.get("/error", function (req, res) {
+  throw new Error("oops I made a mistake");
+});
+
+app.use(function (req, res) {
+  res.status(404).send("Not found");
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err.message);
+});
+
 app.listen(3000, function () {
   console.log("app listening on port 3000");
 });
